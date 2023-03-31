bf5350acd334c77f28dc5fef6cf9bbe5f5aea37e
kon_k <kon_kostas@windowslive.com>
2023-03-31 23:13:59 +0300

    Initial commit

---
+++ src/test/java/gr/uom/thesis/ThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.uom.thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
